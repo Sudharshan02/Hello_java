c421a449cf5d07d10000f09e439ca14de4d80ba5
Sudharshan02 <60228673+Sudharshan02@users.noreply.github.com>
2022-10-27 02:05:15 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
-  public static void main(string[] args) {
+  public static void main(String[] args) {
     // prints "hello, World" in the terminal window.
     System.out.println("Hello, World");
   }
